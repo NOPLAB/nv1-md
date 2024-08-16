1e3537fdef6c19e48f3cbae548323d31eeb6432a
nop <noplab90@gmail.com>
2024-08-17 07:05:12 +0900

    USART3

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,32 @@ use panic_halt as _;
 use {defmt_rtt as _, panic_probe as _};
 
 use embassy_executor::Spawner;
-use embassy_stm32::gpio::{Level, Output, Speed};
+use embassy_stm32::{
+    bind_interrupts,
+    gpio::{Level, Output, Speed},
+};
+use embassy_stm32::{
+    peripherals,
+    usart::{self, Config, Uart},
+};
 use embassy_time::{Duration, Timer};
 use fmt::info;
+use fmt::unwrap;
+
+bind_interrupts!(struct Irqs {
+    USART3 => usart::InterruptHandler<peripherals::USART3>;
+});
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let p = embassy_stm32::init(Default::default());
     let mut led = Output::new(p.PB7, Level::High, Speed::Low);
+
+    let config = Config::default();
+    let mut usart = Uart::new(
+        p.USART3, p.PC5, p.PB10, Irqs, p.DMA1_CH3, p.DMA1_CH4, config,
+    )
+    .unwrap();
 
     loop {
         info!("Hello, World!");
@@ -24,5 +42,7 @@ async fn main(_spawner: Spawner) {
         Timer::after(Duration::from_millis(500)).await;
         led.set_low();
         Timer::after(Duration::from_millis(500)).await;
+
+        unwrap!(usart.write("Hello DMA!".as_bytes()).await);
     }
 }
